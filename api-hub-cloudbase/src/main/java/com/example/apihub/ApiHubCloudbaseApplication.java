242204c808fe1bae6a71ceb8568144fb5483b6e7
weiming <weiming@corp.netease.com>
2023-03-07 23:52:22 +0800

    添加 Cloudbase

---
+++ api-hub-cloudbase/src/main/java/com/example/apihub/ApiHubCloudbaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.apihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHubCloudbaseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiHubCloudbaseApplication.class, args);
+  }
+
+}
